efaf545a2b07e12a3fe3219715911551751da0ba
Balaji P <b8024757@gmail.com>
2026-08-06 12:25:56 +0530

    Update HeroStats.tsx

--- src/components/hero/HeroStats.tsx
+++ src/components/hero/HeroStats.tsx
@@ -3,10 +3,9 @@ import CountUp from "react-countup";
 export default function HeroStats() {
   return (
     <div className="mt-14 flex flex-wrap gap-10">
-
       <div>
         <h3 className="text-4xl font-bold text-blue-400">
-          <CountUp end={15} duration={2} />+
+          <CountUp end={10} duration={2} />+
         </h3>
         <p className="text-slate-400">Projects</p>
       </div>
@@ -20,11 +19,14 @@ export default function HeroStats() {
 
       <div>
         <h3 className="text-4xl font-bold text-blue-400">
-          <CountUp end={2026} duration={2} />
+          <CountUp
+            end={2026}
+            duration={2}
+            separator=""
+          />
         </h3>
         <p className="text-slate-400">Graduate</p>
       </div>
-
     </div>
   );
 }
